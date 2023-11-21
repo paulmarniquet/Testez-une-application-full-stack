fba72c0ad03261b6368cf2daa407dc0c8528a955
paulmarniquet <paul.marniquet@epitech.eu>
2023-11-21 13:12:08 +0100

    e2e tests : Session creation done

--- front/cypress/e2e/session.cy.ts
+++ front/cypress/e2e/session.cy.ts
@@ -1,9 +1,7 @@
 describe('Session spec', () => {
   beforeEach(() => {
     cy.login('yoga@studio.com', 'test!1234');
-  });
 
-  it('Create session successfully', () => {
 
     cy.intercept('GET', '/api/teacher', {
       body: [
@@ -14,6 +12,14 @@ describe('Session spec', () => {
         }
       ]
     },);
+
+
+    cy.get('.mat-card-header > .mat-focus-indicator').click();
+
+  });
+
+
+  it('Create session successfully', () => {
 
     cy.intercept(
       {
@@ -32,18 +38,42 @@ describe('Session spec', () => {
         users: [],
         createdAt: '2021-09-27T14:00:00.000Z',
         updatedAt: '2021-09-27T14:00:00.000Z',
-
       },
     })
-
-    cy.get('.mat-card-header > .mat-focus-indicator').click();
     cy.get('input[formControlName=name]').type("Sandrine")
     cy.get('input[formControlName=date]').type('2023-12-12')
     cy.get('mat-select[formControlName=teacher_id]').click().get('mat-option').contains('Margot DELAHAYE').click();
     cy.get('textarea[formControlName=description]').type(`${"Séance de repos"}{enter}{enter}`)
     cy.get('button[type=submit]').click();
-
     cy.get('.mat-simple-snackbar').should('contain', 'Session created !');
+  });
+
+  it('Empty name', () => {
+    cy.get('input[formControlName=date]').type('2023-12-12')
+    cy.get('mat-select[formControlName=teacher_id]').click().get('mat-option').contains('Margot DELAHAYE').click();
+    cy.get('textarea[formControlName=description]').type(`${"Séance de repos"}{enter}{enter}`)
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
+  it('Empty date', () => {
+    cy.get('input[formControlName=name]').type("Sandrine")
+    cy.get('mat-select[formControlName=teacher_id]').click().get('mat-option').contains('Margot DELAHAYE').click();
+    cy.get('textarea[formControlName=description]').type(`${"Séance de repos"}{enter}{enter}`)
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
+  it('Empty teacher', () => {
+    cy.get('input[formControlName=name]').type("Sandrine")
+    cy.get('input[formControlName=date]').type('2023-12-12')
+    cy.get('textarea[formControlName=description]').type(`${"Séance de repos"}{enter}{enter}`)
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
+  it('Empty description', () => {
+    cy.get('input[formControlName=name]').type("Sandrine")
+    cy.get('input[formControlName=date]').type('2023-12-12')
+    cy.get('mat-select[formControlName=teacher_id]').click().get('mat-option').contains('Margot DELAHAYE').click();
+    cy.get('button[type=submit]').should('be.disabled');
   });
 });
 
