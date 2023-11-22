0c28233681443555eaea2176de41f1671fe28f69
paulmarniquet <paul.marniquet@epitech.eu>
2023-11-22 12:15:59 +0100

    Setting up JAVA tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTests.java
@@ -0,0 +1,53 @@
+package com.openclassrooms.starterjwt.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.openclassrooms.starterjwt.exception.BadRequestException;
+import com.openclassrooms.starterjwt.exception.NotFoundException;
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.SessionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class SessionServiceTests {
+
+    @Mock
+    private SessionRepository sessionRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private SessionService sessionService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void create() {
+        // Arrange
+        Session session = new Session();
+
+        // Act
+        sessionRepository.save(session);
+
+        // Assert
+        verify(sessionRepository, times(1)).save(session);
+    }
+}
