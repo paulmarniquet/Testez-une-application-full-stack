8280347ec569e597ab0155756fbff3d25b496c77
paulmarniquet <paul.marniquet@epitech.eu>
2023-11-25 22:57:00 +0100

    JAVA Tests : security package test started..

---
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsImplTest.java
@@ -0,0 +1,58 @@
+package com.openclassrooms.starterjwt.security.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDetailsImplTest {
+
+    @Test
+    public void isAccountNonExpiredTest() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertTrue(userDetailsImpl.isAccountNonExpired());
+    }
+
+    @Test
+    public void isAccountNonLocked() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertTrue(userDetailsImpl.isAccountNonLocked());
+    }
+
+    @Test
+    public void isCredentialsNonExpired() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertTrue(userDetailsImpl.isCredentialsNonExpired());
+    }
+
+    @Test
+    public void isEnabled() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertTrue(userDetailsImpl.isEnabled());
+    }
+
+    @Test
+    public void getAuthorities() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertEquals(userDetailsImpl.getAuthorities(), new HashSet<GrantedAuthority>());
+    }
+
+    @Test
+    public void equals() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertEquals(userDetailsImpl, userDetailsImpl);
+    }
+
+    @Test
+    public void equalsNull() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertFalse(userDetailsImpl == null);
+    }
+
+    @Test
+    public void equalsOtherObject() {
+        UserDetailsImpl userDetailsImpl = UserDetailsImpl.builder().build();
+        assertFalse(userDetailsImpl.equals(new Object()));
+    }
+}
---
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.starterjwt.security.services;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserDetailsServiceImplTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @Test
+    @Transactional
+    public void loadUserByUsername() {
+
+        // Arrange
+        String username = "Paul";
+        User user = User.builder()
+                .id(1L)
+                .email("paul.marniquet@gmail.com")
+                .firstName("Paul")
+                .lastName("Marniquet")
+                .password("password").build();
+
+        when(userRepository.findByEmail(username)).thenReturn(Optional.ofNullable(user));
+
+        // Act
+        UserDetailsServiceImpl userDetailsService = new UserDetailsServiceImpl(userRepository);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UserDetailsImpl userDetailsImpl = (UserDetailsImpl) userDetails;
+
+        // Assert
+        assert userDetailsImpl.getId().equals(user.getId());
+    }
+}
