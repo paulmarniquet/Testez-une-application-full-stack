09ba8161cd6c783005ac23fb76ad23569392b65e
paulmarniquet <paul.marniquet@epitech.eu>
2023-12-05 22:43:30 +0100

    Integration tests : session controller done

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerIntTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerIntTest.java
@@ -1,14 +1,21 @@
 package com.openclassrooms.starterjwt.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.dto.SessionDto;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.sql.Date;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -19,9 +26,151 @@ public class SessionControllerIntTest {
     private MockMvc mockMvc;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Test
-    public void testGetById() throws Exception {
-        mockMvc.perform(post("/api/session/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    @Autowired
+    private JwtUtils jwtUtils;
+    private String jwt;
+
+    @BeforeEach
+    public void setup() {
+        UserDetailsImpl userDetails = UserDetailsImpl.builder().username("yoga@studio.com").build();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null);
+        jwt = jwtUtils.generateJwtToken(authentication);
     }
 
+
+    @Test
+    public void testFindById() throws Exception {
+        mockMvc.perform(get("/api/session/5")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testFindByIdBadRequest() throws Exception {
+        mockMvc.perform(get("/api/session/notAnId")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testFindByIdNotFound() throws Exception {
+        mockMvc.perform(get("/api/session/0")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        mockMvc.perform(get("/api/session")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setId(1L);
+        sessionDto.setName("test");
+        sessionDto.setDescription("test");
+        sessionDto.setTeacher_id(1L);
+        sessionDto.setDate(Date.valueOf("2023-05-01"));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/session")
+                        .header("Authorization", "Bearer " + jwt)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testCreateBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/session")
+                        .header("Authorization", "Bearer " + jwt)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new SessionDto())))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setId(1L);
+        sessionDto.setName("updated");
+        sessionDto.setDescription("updated session");
+        sessionDto.setTeacher_id(1L);
+        sessionDto.setDate(Date.valueOf("2023-05-01"));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/session/5")
+                        .header("Authorization", "Bearer " + jwt)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdateNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/session/0")
+                        .header("Authorization", "Bearer " + jwt)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new SessionDto())))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUpdateBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/session/notAnId")
+                        .header("Authorization", "Bearer " + jwt)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new SessionDto())))
+                .andExpect(status().isBadRequest());
+    }
+
+/*    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/session/4")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isOk());
+    }*/
+
+    @Test
+    public void testDeleteNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/session/0")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/session/notAnId")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testParticipate() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/session/5/participate/1")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testParticipateNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/session/0/participate/0")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testParticipateBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/session/notAnId/participate/notAnId")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testNoLongerParticipate() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/session/5/participate/3")
+                        .header("Authorization", "Bearer " + jwt))
+                .andExpect(status().isOk());
+    }
 }
